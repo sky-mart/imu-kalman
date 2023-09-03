205eb6dfc7c39fadec51b5968e9fc43b570819e6
Vlad <sky-mart@hotmail.com>
2023-09-03 15:21:05 +0200

    Read CTRL_REG_1_A of LSM303DLHC via I2C

--- Src/main.c
+++ Src/main.c
@@ -43,6 +43,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart2;
@@ -58,6 +60,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_I2C1_Init(void);
 void StartDefaultTask(void const * argument);
 
 /* USER CODE BEGIN PFP */
@@ -71,6 +74,9 @@ void GYRO_CS_Low();
 void GYRO_CS_High();
 uint8_t GYRO_Read(uint8_t address);
 void GYRO_Write(uint8_t address, uint8_t value);
+
+uint8_t ACCEL_Read(uint8_t address);
+void ACCEL_Write(uint8_t address, uint8_t value);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -83,6 +89,8 @@ CONSOLE_COMMAND_DEF(led, "Sets a LED No",
 );
 
 CONSOLE_COMMAND_DEF(spi, "Read whoami of L3GD20");
+
+CONSOLE_COMMAND_DEF(i2c, "Read CTRL_REG_1_A of LSM303DLHC");
 /* USER CODE END 0 */
 
 /**
@@ -115,6 +123,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_SPI1_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   Console_Init();
@@ -177,10 +186,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
@@ -202,12 +212,61 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -300,6 +359,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOE, GYRO_CS_Pin|LD4_Pin|LD3_Pin|LD5_Pin
@@ -365,6 +425,7 @@ void Console_Init()
   console_command_register(hello);
   console_command_register(led);
   console_command_register(spi);
+  console_command_register(i2c);
 }
 
 static void hello_command_handler(const hello_args_t* args)
@@ -400,7 +461,15 @@ static void spi_command_handler(const spi_args_t* args)
 {
     char str[20];
     const uint8_t whoami = GYRO_Read(0xF);
-    snprintf(str, sizeof(str), "whoami=%X\n", whoami);
+    snprintf(str, sizeof(str), "whoami=0x%X\n", whoami);
+    Console_Write(str);
+}
+
+static void i2c_command_handler(const i2c_args_t* args)
+{
+    char str[20];
+    const uint8_t ctrl_reg_1_a = ACCEL_Read(0x20);
+    snprintf(str, sizeof(str), "ctrl_reg_1_a=0x%X\n", ctrl_reg_1_a);
     Console_Write(str);
 }
 
@@ -427,6 +496,17 @@ uint8_t GYRO_Read(uint8_t address)
     return value;
 }
 /* void GYRO_Write(uint8_t address, uint8_t value); */
+
+uint8_t ACCEL_Read(uint8_t address)
+{
+    const uint16_t ACCEL_READ_ADDR = 0x33;
+    uint8_t value;
+    // TODO: handle error codes
+    HAL_I2C_Mem_Read(&hi2c1, ACCEL_READ_ADDR, (uint16_t)address, sizeof(address), &value, sizeof(value), 10);
+    return value;
+}
+
+/* void ACCEL_Write(uint8_t address, uint8_t value); */
 
 /* USER CODE END 4 */
 
